581f797ca31811886e04ddaf4bad4ad884a6dbec
Omolade Hassan <omoladehassan996@gmail.com>
2022-08-18 19:45:24 +0100

    added the country code selector

--- practise.js
+++ practise.js
@@ -1,13 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const mtnNumbers = ["0803", "0806", "0704", "0703", "0706", "0813", "0816", "0814", "0903", "0906", "0810", "0913", "0916"];
-  const airtelNumbers = ["0802", "0808", "0702", "0708", "0812", "0701", "0901", "0818", "0902", "0904", "0912", "0907"];
+  const mtnNumbers = [
+    "0803",
+    "0806",
+    "0704",
+    "0703",
+    "0706",
+    "0813",
+    "0816",
+    "0814",
+    "0903",
+    "0906",
+    "0810",
+    "0913",
+    "0916",
+  ];
+  const airtelNumbers = [
+    "0802",
+    "0808",
+    "0702",
+    "0708",
+    "0812",
+    "0701",
+    "0901",
+    "0818",
+    "0902",
+    "0904",
+    "0912",
+    "0907",
+  ];
   const gloNumbers = ["0805", "0807", "0705", "0815", "0811", "0905", "0915"];
-  const nineMobileNumbers = ["0809", "0818", "0817", "0909", "0908"]
+  const nineMobileNumbers = ["0809", "0818", "0817", "0909", "0908"];
 
   console.log("happier baby: loaded!!", Date.now());
-  const phoneNumber = document.getElementById("phone_number")
-  const button = document.getElementById("button")
-  const img = document.getElementById("img")
+  const phoneNumber = document.getElementById("phone_number");
+  const button = document.getElementById("button");
+  const img = document.getElementById("img");
 
   function validatePhoneNumber() {
     const value = phoneNumber.value; // will be more than 4 characters
@@ -20,18 +47,22 @@ document.addEventListener("DOMContentLoaded", () => {
     // console.log('mtnBool', mtnBool)
 
     if (mtnBool) {
-      img.src = './images/MTN.png'
-    } else  if  (airtelBool) {
-      img.src = './images/Airtel.png'
+      img.src = "./images/MTN.png";
+      img.style.visibility = "visible";
+    } else if (airtelBool) {
+      img.src = "./images/Airtel.png";
+      img.style.visibility = "visible";
+    } else if (gloBool) {
+      img.src = "./images/Glo.png";
+      img.style.visibility = "visible";
+    } else if (nineMobileBool) {
+      img.src = "./images/9mobile.png";
+      img.style.visibility = "visible";
+    } else {
+      img.style.visibility = "hidden";
     }
-    else if (gloBool) {
-      img.src = './images/Glo.png'
-    }
-    else if (nineMobileBool) 
-    {img.src = './images/9mobile.png'}
   }
 
-  button.addEventListener("click", validatePhoneNumber)
+  button.addEventListener("click", validatePhoneNumber);
   phoneNumber.addEventListener("keyup", validatePhoneNumber);
-
-})
+});
